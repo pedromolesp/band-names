8ee92fe85e3b2b64fc58fb25a4a3936136b6027d
pedro.molina <pedromolesp@gmail.com>
2020-11-22 15:39:54 +0100

    añadido modelo básico de banda

---
+++ lib/pages/home.dart
@@ -0,0 +1,55 @@
+import 'package:band_names/models/band.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Band> bands = [
+    Band(id: "1", name: "Metallica", votes: 2),
+    Band(id: "2", name: "Sum 41", votes: 3),
+    Band(id: "3", name: "Bon Jovi", votes: 5),
+    Band(id: "4", name: "Breaking Benjamin", votes: 5),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        title: Text(
+          'BandNames',
+          style: TextStyle(color: Colors.black87),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+          itemCount: bands.length,
+          itemBuilder: (context, index) => bandTile(bands[index])),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        elevation: 1,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  ListTile bandTile(Band band) {
+    return ListTile(
+      leading: CircleAvatar(
+        child: Text(band.name.substring(0, 2)),
+        backgroundColor: Colors.blue[100],
+      ),
+      title: Text(band.name),
+      trailing: Text(
+        '${band.votes}',
+        style: TextStyle(fontSize: 20),
+      ),
+      onTap: () {
+        print(band.name);
+      },
+    );
+  }
+}
